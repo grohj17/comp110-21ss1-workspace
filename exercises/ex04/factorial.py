2cc29ad60b949fd0f900547b64b20fe368451041
Jay Groh <grohj@live.unc.edu>
2021-05-24 18:33:46 -0400

    finshed ex04

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -1,6 +1,12 @@
 """An exercise in computing the factorial of an int."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730201179"
 
 
-# Begin your solution here...
+number: int = int(input("Choose a number: "))
+i: int = 1
+fac: int = 1
+while number >= i:
+    fac = fac * i
+    i = i + 1  
+print("Factorial: " + str(fac))
--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -1,6 +1,20 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730201179"
 
 
-# Begin your solution here...
+beat: str = input("What beat do you want to repeat? ")
+repetition: int = int(input("How many times do you want to repeat it? "))
+i: int = 0
+x: str = " "
+if repetition <= 0:
+    print("No beat...")
+else:
+    while i <= repetition:
+        if i <= 0:
+            x = beat
+            i = i + 1
+        else:
+            x = x + " " + beat
+        i = i + 1
+    print(x)
